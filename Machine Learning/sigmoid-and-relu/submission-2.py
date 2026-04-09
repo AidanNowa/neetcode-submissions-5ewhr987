675a864540518a15f3429310a761b6212c4050fa
AidanNowa <98485635+AidanNowa@users.noreply.github.com>
2026-04-08 23:57:28 -0400

    Add: sigmoid-and-relu - submission-2

---
+++ Machine Learning/sigmoid-and-relu/submission-2.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: 1 / (1 + e^(-z))
+        # return np.round(your_answer, 5)
+        # sigmoid_array = []
+        # for ele in z:
+            # sigmoid_array.append(round((1/(1+np.exp(-1*ele))),5))
+        # return sigmoid_array
+        return np.round((1/(1+np.exp(-1*z))),5)
+
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: max(0, z) element-wise
+        # relu_array = []
+        # for ele in z:
+            # relu_array.append(round(max(0.0,ele),5))
+        # return relu_array
+        return np.maximum(0.0,z)
