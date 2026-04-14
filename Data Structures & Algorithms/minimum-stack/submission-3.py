cc5bfae6316fc368cf45314bf571e00fc21a72c0
AidanNowa <98485635+AidanNowa@users.noreply.github.com>
2026-04-14 11:10:23 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.values = []
+        self.curr_min = float('inf')
+        
+
+    def push(self, val: int) -> None:
+        self.values.append(val)
+        if val < self.curr_min:
+            self.curr_min = val
+        
+
+    def pop(self) -> None:
+        if self.values[-1] == self.curr_min:
+            if len(self.values[:-1]) == 0:
+                self.curr_min = float('inf')
+            else:
+                self.curr_min = min(self.values[:-1])
+        self.values = self.values[:-1]
+
+    def top(self) -> int:
+        return self.values[-1]
+ 
+        
+    def getMin(self) -> int:
+        return self.curr_min
+        
